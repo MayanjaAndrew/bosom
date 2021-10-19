bb84261ea65b810b953b5cdc01e5791865105c2d
MayanjaAndrew <andremayanja@gmail.com>
2021-10-19 18:00:33 +0300

    login

--- src/pages/VIP/Login.js
+++ src/pages/VIP/Login.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './login.css';
+import Logo from './ASEI-3D-Logo.png';
 
 
 function Login() {
     return (
-        <div>
-        <p>Hello</p>
+        <>
+        <section className="Login__section">
+         <div className="Login__container">
+        <div className="Login__split">
+        <div className="Login__image"></div>
         </div>
+        </div>
+        </section>
+        </>
     )
 }
 
